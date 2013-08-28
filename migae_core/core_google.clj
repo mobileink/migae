a4dbc6742628e6e38958cbe4e38959f0f0e41c92
Gregg Reynolds <dev@mobileink.com>
2013-08-28 05:46:29 -0500

    remove migae_core

--- migae_core/core_google.clj
+++
@@ -1,6 +0,0 @@
-(in-ns 'migae.migae-kernel.core)
-
-;(import '[java.io File FileInputStream BufferedInputStream])
-
-(defmacro def-migae-app [app-var-name handler & [args]]
-  `(def ~app-var-name ~handler))
