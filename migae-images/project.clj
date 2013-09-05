2481f849b7b317002424090486a0b12875d0d692
Gregg Reynolds <dev@mobileink.com>
2013-09-05 07:22:54 -0500

    fix :dependencies

--- migae-images/project.clj
+++ migae-images/project.clj
@@ -2,4 +2,9 @@
   :description "migae - Mobile Ink Google App Engine sdk for Clojure."
   :url "https://github.com/greynolds/migae"
   :min-lein-version "2.0.0"
-  :dependencies [[migae/migae-core "0.1.0-SNAPSHOT"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.google.appengine/appengine-api-1.0-sdk "1.8.3"]]
+  :profiles {:test
+             {:dependencies [[com.google.appengine/appengine-api-1.0-sdk "1.8.3"]
+                             [com.google.appengine/appengine-api-stubs "1.8.3"]
+                             [com.google.appengine/appengine-testing "1.8.3"]]}})
--- migae-memcache/project.clj
+++ migae-memcache/project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/greynolds/migae"
   :min-lein-version "2.0.0"
   :aot [#".*"]
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.google.appengine/appengine-api-1.0-sdk "1.8.3"]]
   :profiles {:test
              {:dependencies [[com.google.appengine/appengine-api-1.0-sdk "1.8.3"]
                              [com.google.appengine/appengine-api-stubs "1.8.3"]
