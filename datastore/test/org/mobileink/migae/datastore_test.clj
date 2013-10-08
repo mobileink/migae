03fa3b9a8abdc568d0ed23444c7ba331f407fba2
Gregg Reynolds <dev@mobileink.com>
2013-10-08 09:50:03 -0500

    ds: rename test stuff to org.mobileink.migae

--- datastore/test/org/mobileink/migae/datastore_test.clj
+++ datastore/test/org/mobileink/migae/datastore_test.clj
@@ -1,4 +1,4 @@
-(ns migae.datastore-test
+(ns org.mobileink.migae.datastore-test
   (:refer-clojure :exclude [name hash])
   (:import [com.google.appengine.tools.development.testing
             LocalServiceTestHelper
