beaf0691b609e91c14c2002ae1c6d7580b2e8ab1
Gregg Reynolds <dev@mobileink.com>
2013-10-08 09:56:19 -0500

    mc: fix proj dependencies

--- memcache/project.clj
+++ memcache/project.clj
@@ -1,17 +1,22 @@
-(defproject migae/migae-memcache "0.1.0-SNAPSHOT"
+(defproject org.mobileink.migae/memcache "0.1.0-SNAPSHOT"
   :description "migae - Mobile Ink Google App Engine sdk for Clojure."
   :url "https://github.com/greynolds/migae"
   :min-lein-version "2.0.0"
   :aot [#".*"]
   :test-selectors {:stats :stats
                    :put :put
+                   :miss :miss
                    :del :del
                    :incr :incr}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.google.appengine/appengine-api-1.0-sdk "1.8.3"]]
-  :profiles {:test
-             {:dependencies [[com.google.appengine/appengine-api-1.0-sdk "1.8.3"]
-                             [com.google.appengine/appengine-api-stubs "1.8.3"]
-                             [com.google.appengine/appengine-testing "1.8.3"]]}})
-
+                 [com.google.appengine/appengine-api-1.0-sdk "1.8.4"]
+                 [log4j "1.2.17" :exclusions [javax.mail/mail
+                                              javax.jms/jms
+                                              com.sun.jdmk/jmxtools
+                                              com.sun.jmx/jmxri]]
+                 [org.slf4j/slf4j-log4j12 "1.6.6"]
+                 [org.clojure/tools.logging "0.2.3"]]
+  :profiles {:test {:dependencies [[com.google.appengine/appengine-api-stubs "1.8.4"]
+                                   [com.google.appengine/appengine-testing "1.8.4"]
+                                   [ring-zombie "1.0.1"]]}})
 
